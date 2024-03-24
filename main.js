f73e08a184e89892006b45f4c19bf7aaf6fa2f6d
Fayyazalam439 <160386224+Fayyazalam439@users.noreply.github.com>
2024-03-25 00:21:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//MATHEMATICAL OPERATORS
+// 1-Addition Operator.
+var num1 = 2;
+var num2 = 6;
+console.log(num1 + num2); //08
+//Subtraction Operator.
+var subtraction1 = 6;
+var subtraction2 = 2;
+console.log(subtraction1 - subtraction2); //04
+//Multiplication Operator.
+var Multiplication1 = 5;
+var Multiplication2 = 5;
+console.log(Multiplication1 * Multiplication2); //25
+//Division Operator.
+var Division1 = 20;
+var Division2 = 4;
+console.log(Division1 / Division2); //05
+//Exponentiation.
+var Exponentiation1 = 6;
+var Exponentiation2 = 4;
+console.log(Math.pow(Exponentiation1, Exponentiation2)); //1296
+//Modulus.
+var Modulus1 = 5;
+var Modulus2 = 2;
+console.log(Modulus1 % Modulus2);
